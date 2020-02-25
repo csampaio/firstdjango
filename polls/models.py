cf802910510b8f388f05e6fe3ab3d4ab531c23ce
Claudio Sampaio <claudio.sampaio@gmail.com>
2020-02-24 23:21:39 -0400

    part 5 done

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -9,8 +10,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recentily(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
